fdf80ca4e5fd07f92d45cc24b4b5b4990c42267c
jomart-gsu <jmartin191@gsu.edu>
2022-03-29 18:15:17 -0400

    removed print statement

--- app.py
+++ app.py
@@ -18,7 +18,6 @@ data = response.json()
 headlines = []
 
 for i in range(0, 10):
-    print(data["response"]["docs"][i]["headline"]["main"])
     headlines.append(data["response"]["docs"][i]["headline"]["main"])
 
 
